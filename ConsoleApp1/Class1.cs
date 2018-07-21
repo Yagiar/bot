fd232c350eac038693b5aced8c31ad8a09a3bd85
Yagiar <alexrumling2000@gmail.com>
2018-07-21 22:56:13 +0300

    Добавил деление
    Ну вот норм тема

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+       static public float delenie(float a,float b)
+        {
+            float result  = a / b;
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a; float b;
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            float Result=Class1.delenie(a,b);
+            Console.WriteLine(Result);
+            
+            Console.ReadKey();
+        }
+    }
+}
